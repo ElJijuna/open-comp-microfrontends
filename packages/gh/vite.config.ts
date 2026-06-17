887c424217a70c63d5063acbdfadea5ce4e44476
ElJijuna <pilmee@icloud.com>
2026-06-17 13:13:57 -0500

    feat: add @open-comp/gh package with configuration files and release setup

---
+++ packages/gh/vite.config.ts
@@ -0,0 +1,62 @@
+import federation from '@originjs/vite-plugin-federation';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import pkg from './package.json';
+
+const isFederation = process.env.BUILD_MODE === 'federation';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    isFederation &&
+      federation({
+        name: 'opencomp_gh',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './GitHubProfile': './src/GitHubProfile.tsx',
+        },
+        shared: {
+          react: {
+            singleton: true,
+            requiredVersion: pkg.peerDependencies.react,
+          },
+          'react-dom': {
+            singleton: true,
+            requiredVersion: pkg.peerDependencies['react-dom'],
+          },
+        },
+      }),
+  ].filter(Boolean),
+
+  build: isFederation
+    ? {
+        target: 'esnext',
+        outDir: 'dist',
+        assetsDir: '',
+        emptyOutDir: false,
+        rollupOptions: {
+          input: './src/index.ts',
+          external: ['react', 'react-dom'],
+        },
+      }
+    : {
+        lib: {
+          entry: 'src/index.ts',
+          name: 'OpenCompGh',
+          formats: ['es', 'umd'],
+          fileName: (fmt) => `index.${fmt}.js`,
+        },
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+            },
+          },
+        },
+        target: 'esnext',
+        outDir: 'dist',
+        emptyOutDir: true,
+      },
+});
